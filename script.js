79b88022b8060b543cbb8c0c1495fb5d25c3f55c
Punsisi99 <punsisichamodha99@gmail.com>
2024-07-08 15:13:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//nav bar scroll
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+
+    //menubar icon
+    let menuIcon = document.querySelector(".menu-icon");
+
+    menuIcon.addEventListener("click",()=>{
+        menuIcon.classList.toggle("active");
+    });
+
+    //counting of portfolio
+    const portfolioContainer = document.querySelector(".portfolio-container");
+    const volunteeringCount = document.getElementById("volunteering");
+    const certificatesCount = document.getElementById("certificates");
+
+    function countProject(){
+        let projects = portfolioContainer.value;
+    }    
+
+    //portfolio filter
+    var mixer = mixitup('.project-container',{
+        selectors: {
+            target: '.project-box'
+        },
+        animation: {
+            duration: 500
+        }
+    });
+     
+});
